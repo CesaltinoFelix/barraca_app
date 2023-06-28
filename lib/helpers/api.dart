a717692916155a2eebd869e0e2efdbe228e0fbdb
Cesaltino Felix <cesaltinofelix2000@gmail.com>
2023-06-28 20:37:55 +0100

    updating code

--- lib/helpers/api.dart
+++ lib/helpers/api.dart
@@ -1,4 +1,4 @@
-const baseUrl = 'http://192.168.100.51:3000';
+const baseUrl = 'http://192.168.100.50:3000';
 // const baseUrl = 'http://10.0.0.118:3000';
 // const baseUrl = 'http://192.168.41.154:3000';
 // const baseUrl = 'http://10.0.0.118:3000';
